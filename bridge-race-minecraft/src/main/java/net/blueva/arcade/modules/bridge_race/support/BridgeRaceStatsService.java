b5c928069dbecfc2ccb7767365ab1e9200f76e8d
Whiron <dnlblasco@gmail.com>
2026-07-28 23:15:55 +0200

    fix: stop writing the core-owned global wins stat

--- bridge-race-minecraft/src/main/java/net/blueva/arcade/modules/bridge_race/support/BridgeRaceStatsService.java
+++ bridge-race-minecraft/src/main/java/net/blueva/arcade/modules/bridge_race/support/BridgeRaceStatsService.java
@@ -47,7 +47,6 @@ public class BridgeRaceStatsService {
         }
 
         statsAPI.addModuleStat(player, moduleInfo.getId(), "wins", 1);
-        statsAPI.addGlobalStat(player, "wins", 1);
     }
 
     public void recordGamePlayed(Collection<Player> players) {
